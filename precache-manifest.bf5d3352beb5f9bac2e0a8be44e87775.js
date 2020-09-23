e7743fe68ee844f17ea241fd0d730196f3e4073f
Vinay Patel <vinay.patel@go-mmt.com>
2020-09-23 15:05:05 +0530

    Updates

---
+++ precache-manifest.bf5d3352beb5f9bac2e0a8be44e87775.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267e0f3807922887e92cd4a752c32712",
+    "url": "/drawing-board/index.html"
+  },
+  {
+    "revision": "d14cb9274c94f6796b8c",
+    "url": "/drawing-board/static/css/main.81e3f1b2.chunk.css"
+  },
+  {
+    "revision": "efad1bba78a0a5c1a065",
+    "url": "/drawing-board/static/js/2.8b0759ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drawing-board/static/js/2.8b0759ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14cb9274c94f6796b8c",
+    "url": "/drawing-board/static/js/main.80877b00.chunk.js"
+  },
+  {
+    "revision": "322d993b5bb28493876f",
+    "url": "/drawing-board/static/js/runtime-main.1cf25a2e.js"
+  },
+  {
+    "revision": "69f073c49d94675f3f82919b9a284d81",
+    "url": "/drawing-board/static/media/dot.69f073c4.png"
+  },
+  {
+    "revision": "3bfa5ce658f6298ca057f98d389d6d96",
+    "url": "/drawing-board/static/media/erase.3bfa5ce6.png"
+  },
+  {
+    "revision": "f127e836a30b8583e787d0475219179b",
+    "url": "/drawing-board/static/media/highlight.f127e836.png"
+  },
+  {
+    "revision": "447a01654f5f237087c2a045c76ceb2c",
+    "url": "/drawing-board/static/media/pen.447a0165.png"
+  }
+]);
